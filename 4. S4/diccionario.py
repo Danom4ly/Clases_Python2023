12f9631ce4121e84e34b8214b1f1c446661e3056
promenade <mirandacanelo.daniel@gmail.com>
2023-04-20 20:52:28 -0400

    Sesion 4

---
+++ 4. S4/diccionario.py
@@ -0,0 +1,10 @@
+#ordenados por clave y valor
+#diccionarios, es una coleccion de elemtnos
+#declaracion con llaves {} o con la funcion dict()
+
+estudiantes = {
+    #clave:valor
+    "falanito":25,
+    "Maria":22,
+    
+}
