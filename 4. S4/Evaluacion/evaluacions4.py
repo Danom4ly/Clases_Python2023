cb50c34536eb7b5b9c0c6bccbdfed9b9b55c47a0
Promenade <mirandacanelo.daniel@gmail.com>
2023-04-23 22:22:08 -0400

    Evaluacion

--- 4. S4/Evaluacion/evaluacions4.py
+++ 4. S4/Evaluacion/evaluacions4.py
@@ -1,9 +1,16 @@
-miDiccionario= {
+""" miDiccionario= {
     #clave:valor
     "persona1":{"Juan", "Perez", 111111111},
     "persona2":{"Fulanito", "Gonzalez", 222222222},
     "persona3":{"Carla", "Vidal", 333333333}
-}
+} """
 
-print(miDiccionario)
+
+persona1 = {"Juan", "Perez", 111111111}
+persona2 = {"Fulanito", "Gonzalez", 222222222}
+persona3 = {"Carla", "Vidal", 333333333}
+
+listaDePersonas = [persona1,persona2,persona3]
+
+print(listaDePersonas)
 
