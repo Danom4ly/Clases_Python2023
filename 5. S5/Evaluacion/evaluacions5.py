c241728047d7228f54e87ce44ac5b4c9d079696e
Promenade <mirandacanelo.daniel@gmail.com>
2023-04-25 00:15:58 -0400

    Avance evaluacion

---
+++ 5. S5/Evaluacion/evaluacions5.py
@@ -0,0 +1,10 @@
+for val in "paralelepipedo":
+    if val == "a":
+        continue
+    elif val == "e":
+        continue
+    elif val == "i":
+        continue
+    elif val == "o":
+        continue
+    print(val)
