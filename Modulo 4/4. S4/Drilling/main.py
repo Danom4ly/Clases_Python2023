06d204d22424ae37e7c118561afbec85eb6fefe4
Promenade <mirandacanelo.daniel@gmail.com>
2023-05-15 23:58:09 -0400

    1505

---
+++ Modulo 4/4. S4/Drilling/main.py
@@ -0,0 +1,12 @@
+from persona import Persona
+from ciclista import Ciclista
+from maratonista import Maratonista
+
+persona_1 = Persona('Juan')
+persona_1.movimiento("Caminando")
+
+ciclista_1 = Ciclista("Pedro")
+ciclista_1.movimiento("Rodando")
+
+maratonista_1 = Maratonista("Fulanito")
+maratonista_1.movimiento("trotando")
